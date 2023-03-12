d8a3fb41f58c320eb5ac2600d11f39d078bcf3e0
Alnaive <astolo86@gmail.com>
2023-03-13 03:24:01 +0800

    update cors

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended:true }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://enter-mate.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
+// Handle preflight requests
+app.options('*', cors(corsOptions));
+  
 
 const db = require("./app/models/index.js");
 const Role = db.role;
